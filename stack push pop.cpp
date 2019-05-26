e7804e67c2281506ed19784a6449f5af84ca9626
unknown <ayvazyan.1997.exp@gmail.com>
2019-05-26 16:03:19 +0400

    as

---
+++ stack push pop.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+template<class T>
+class stack {
+public:
+	stack():size(0), capasity(1),x(nullptr) {}
+	void push(int val)
+	{
+		if (size + 1 < capasity)
+		{
+			x[size++] = val;
+		}
+		else
+		{
+			capasity *= 2;
+			int* crr = new int[capasity];
+			for (int i = 0; i < size; ++i)
+			{
+				crr[i] = x[i];
+			}
+			delete[] x;
+			x = crr;
+			x[size++] = val;
+		
+			crr = nullptr;
+		}
+
+	}
+
+	T top()
+	{
+		return x[size - 1];
+	}
+
+	void pop_front()
+	{
+		x[--size];
+	}
+
+
+
+private:
+	int size;
+	int capasity;
+	int *x;
+};
+
+int main()
+{
+	stack<int> x;
+	x.push(1);
+	x.push(2);
+	x.push(3);
+	x.pop_front();
+	std::cout << x.top() << "\n";
+}
